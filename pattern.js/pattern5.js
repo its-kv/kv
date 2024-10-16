2ad4e151dd1095021851c60440970ef78c1e23ef
kartikeya <kartiikeyvashishtha@gmail.com>
2024-10-16 17:41:13 +0530

    sameday

---
+++ pattern.js/pattern5.js
@@ -0,0 +1,21 @@
+for (let i=0; i<5; i++){
+   let str= "";
+    for (let j=5; j>=0; j--){
+       if(i>=j) {
+        str+= "*";
+       }
+       else {
+        str+= " ";
+       }
+    }
+for (let j=1; j<5; j++){
+if (i>=j){
+    str+= "*";
+}
+else{
+    str+= " ";
+}
+
+}
+console.log(str);
+}
